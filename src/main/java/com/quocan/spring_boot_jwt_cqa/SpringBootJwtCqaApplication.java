15fea3e49f458452023468a3c8a7d43953ec296e
chauquocan <chauquocan2090@gmail.com>
2021-09-22 08:26:27 +0700

    commit branch master

---
+++ src/main/java/com/quocan/spring_boot_jwt_cqa/SpringBootJwtCqaApplication.java
@@ -0,0 +1,13 @@
+package com.quocan.spring_boot_jwt_cqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtCqaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtCqaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quocan/spring_boot_jwt_cqa/SpringBootJwtCqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quocan.spring_boot_jwt_cqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtCqaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
